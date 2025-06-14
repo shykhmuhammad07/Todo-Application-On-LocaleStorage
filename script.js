9904640a4b1df1481a66f440e2969bb744e800d6
shykhmuhammad <muhammmadsheikh880@gmail.com>
2025-06-14 15:39:55 +0500

    done

--- script.js
+++ script.js
@@ -1,20 +1,63 @@
 {
-var getList = document.getElementById('list');
-var getInput = document.getElementById('todo');
-function addtodo() {
-    if(getInput.value === ''){
-    alert('Fill the field')
-    }else{
-    getList.innerHTML += `<li class = 'list-1'> ${getInput.value} <button class = 'btn-3' onclick = 'this.parentNode.remove()'>Delete</button> <button class = 'btn-4' onclick= 'editItem(this)'> Edit </button> </li>`;
-    getInput.value = '';
-}}
-function del() {
-    getList.innerHTML = ''
-}
-function editItem(e){
-    var xyz = prompt('enter the value',e.parentNode.firstChild.textContent )
-    e.parentNode.firstChild.textContent = xyz 
-}
-}
+  var getList = document.getElementById("list");
+  var getInput = document.getElementById("todo");
 
+  window.onload = function () {
+    const data = JSON.parse(localStorage.getItem("data")) || [];
+    data.forEach((todo, index) => {
+      addToDom(todo, index);
+    });
+  };
 
+  function addToDom(todo) {
+    getList.innerHTML += `<li class = 'list-1'> ${todo} <button class = 'btn-3' onclick = 'delItem(this)'><i class="fas fa-trash"></i>
+    </button> <button class = 'btn-4' onclick= 'editItem(this)'><i class="fas fa-edit"></i></button> </li>`;
+  }
+
+  function addtodo() {
+    let data = JSON.parse(localStorage.getItem("data")) || [];
+    let todoText = getInput.value.trim();
+    if (!todoText) {
+      alert("Fill the field");
+    } else {
+      data.push(todoText);
+      localStorage.setItem("data", JSON.stringify(data));
+      addToDom(todoText, data.length - 1);
+      getInput.value = "";
+    }
+  }
+
+  function delItem(event) {
+    const list = event.parentNode;
+    const lists = Array.from(getList.children);
+    let index = lists.indexOf(list);
+    list.remove();
+    let data = JSON.parse(localStorage.getItem("data")) || [];
+    data.splice(index, 1);
+    localStorage.setItem("data", JSON.stringify(data));
+  }
+
+  function del() {
+    getList.innerHTML = "";
+    localStorage.removeItem("data");
+  }
+
+  function editItem(button) {
+    let data = JSON.parse(localStorage.getItem("data")) || [];
+
+    let listItem = button.parentNode;
+
+    let listItems = Array.from(getList.children);
+    let index = listItems.indexOf(listItem);
+
+    let currentValue = data[index];
+    let updatedValue = prompt("Enter the new value", currentValue);
+
+    if (updatedValue !== null && updatedValue.trim() !== "") {
+      data[index] = updatedValue.trim();
+      localStorage.setItem("data", JSON.stringify(data));
+
+      listItem.childNodes[0].nodeValue = updatedValue + " ";
+    }
+  }
+}
